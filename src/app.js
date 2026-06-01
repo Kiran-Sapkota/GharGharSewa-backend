1a7e4e9994b8990a67e21eb7c0b34cb40cc3685d
Kiran-Sapkota <kiransapcr7@gmail.com>
2026-06-02 00:33:24 +0545

    I hope this fixes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const path = require("path");
 const mongoose = require("mongoose");
 
+require("dotenv").config();
+
 const authRoutes = require("./routes/authRoutes");
 const providerRoutes = require("./routes/providerRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
@@ -13,6 +15,25 @@ const recommendationRoutes = require("./routes/recommendationRoutes");
 const chatbotRoutes = require("./routes/chatbotRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const serviceCategoryRoutes = require("./routes/serviceCategoryRoutes");
+
+const MONGO_URI = process.env.MONGO_URI;
+
+if (MONGO_URI) {
+  mongoose
+    .connect(MONGO_URI, {
+      tls: true,
+      tlsAllowInvalidCertificates: true,
+      tlsAllowInvalidHostnames: true,
+    })
+    .then(() => {
+      console.log("MongoDB Connected inside app.js");
+    })
+    .catch((err) => {
+      console.error("DB Connection Error inside app.js:", err.message);
+    });
+} else {
+  console.error("MONGO_URI is missing in environment variables");
+}
 
 const app = express();
 
@@ -42,16 +63,19 @@ app.get("/", (req, res) => {
   const envPath = path.join(__dirname, "..", ".env");
   const isEnvPresent = fs.existsSync(envPath);
   const dbState = mongoose.connection.readyState;
-  console.log(dbState)
   let dbStatus = "Disconnected";
+  
   if (dbState === 1) dbStatus = "Connected";
   else if (dbState === 2) dbStatus = "Connecting";
   else if (dbState === 3) dbStatus = "Disconnecting";
-
+  
+  // Return more detailed info as requested
   res.json({
     message: "GharGhar Sewa Backend is bro and its running good i guess",
     envFilePresent: isEnvPresent,
     databaseStatus: dbStatus,
+    dbStateCode: dbState, // 0: disconnected, 1: connected, 2: connecting, 3: disconnecting
+    mongoUriConfigured: !!process.env.MONGO_URI, // Check if the environment variable is loaded
   });
 });
 
